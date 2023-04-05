194875c8cf4ecc1fafbc68681ff0270e95171fbf
krandon1986 <krandon1986@yahoo.co.uk>
2023-04-05 15:25:36 +0000

    no comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$('p').click(function(){
-    $('p').slideToggle('slow');
-});
+$(document).ready(function(){
+    $(".box").on("click", function(){
+        var classNames = $(this).attr("class").split(" ");
+        var boxClass = classNames[0];
+        var classNames = classNames[1];
+        if($(this).css("background-color") == "rgb(255, 0, 0)") {
+            $("." + classNames).css("background-color", "#000");
+        } else {
+            $("." + boxClass).css("background-color", "#000");
+            $("." + classNames).css("background-color", "red");
+        }
+        });
+    });
